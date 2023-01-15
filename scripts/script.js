409b7e2e6a121d09414c54c1cd6f35b563078ad0
VolNa <grizli887@mail.ru>
2023-01-15 21:27:43 +0300

    slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
-const burger = document.querySelector('.burger');
+const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
 const burgerIcon = document.querySelector(".burger__icon");
 
 const showBurger = () => {
-    burger.classList.toggle('active');
-    nav.classList.toggle("active");
-}
-burgerIcon.addEventListener('click', showBurger);
-
+	burger.classList.toggle("active");
+	nav.classList.toggle("active");
+};
+burgerIcon.addEventListener("click", showBurger);
 
 function onEntry(entry) {
 	entry.forEach((change) => {
@@ -22,3 +21,47 @@ let elements = document.querySelectorAll(".animation");
 for (let elm of elements) {
 	observer.observe(elm);
 }
+
+let skillsArr = [];
+
+Array.from(document.querySelectorAll(".a")).map((item) => {
+	skillsArr.push(item.textContent);
+	item.remove();
+});
+
+let step = 0;
+let offset = 0;
+
+const draw = () => {
+	let skill = document.createElement("li");
+	skill.textContent = skillsArr[step];
+	skill.classList.add("a");
+	skill.style.left = offset * 10 + "vw";
+	document.querySelector(".skills__list").appendChild(skill);
+	step + 1 === skillsArr.length ? (step = 0) : step++;
+	offset++;
+};
+
+for (let i = 0; i < 5; i++) {
+	draw()
+}
+
+const moveLeft = () => {
+	let allSkills = Array.from(document.querySelectorAll(".a"));
+	let secondOffset = 0;
+	allSkills.map((item) => {
+		item.style.left = secondOffset * 10 + "vw";
+		secondOffset++;
+	});
+	allSkills[0].classList.add('transparent')
+	allSkills[allSkills.length-1].classList.add("transparent");
+	allSkills[allSkills.length - 1].classList.remove("transparent");
+	setTimeout(() => {
+		
+		allSkills[0].remove();
+	}, 1000);
+	
+};
+
+document.querySelector(".skills").addEventListener("click", draw);
+document.querySelector(".skills").addEventListener("click", moveLeft);
